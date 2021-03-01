a8c639eee7758fe1f4bd41a63e0b593adb0cff6e
ASC IT Solutions <andrew@asc-solutions.co.uk>
2021-03-01 10:11:46 +0000

    Add files via upload

---
+++ test/go.mod
@@ -0,0 +1,8 @@
+module dev.azure.com/asc-infrastructure/IAC/_git/terraform-azurerm-resourcegroup
+
+go 1.15
+
+require (
+	github.com/gruntwork-io/terratest v0.32.8
+	github.com/stretchr/testify v1.7.0
+)
---
+++ test/terraform_basic_example_test.go
@@ -0,0 +1,29 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTerraformResourceGroupExample(t *testing.T) {
+	// Construct the terraform options with default re-tryable errors to handle the most common
+	// retryable errors in terraform testing.
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		// Set the path to the Terraform code that will be tested.
+		TerraformDir: "../examples",
+	})
+
+	// Clean up resources with "terraform destroy" at the end of the test.
+	defer terraform.Destroy(t, terraformOptions)
+
+	// Run "terraform init" and "terraform apply". Fail the test if there are any errors.
+	terraform.InitAndApply(t, terraformOptions)
+
+	// Run 'terraform output' to get the values of the output variables and check they match.
+	rgName := terraform.Output(t, terraformOptions, "rg_name")
+	rgLocation := terraform.Output(t, terraformOptions, "rg_location")
+	assert.Equal(t, "rg-dev-testing", rgName)
+	assert.Equal(t, "uksouth", rgLocation)
+}
